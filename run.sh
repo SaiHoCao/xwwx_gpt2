0ada7db18264d450551b13e13c9c569aeba385ae
saihocao <pyfirstcsh@gmail.com>
2025-07-18 02:58:24 +0000

    add

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 python run_clm.py \
-    --model_name_or_path ../../gpt2-medium \
+    --model_name_or_path ../../Meta-Llama-3-8B-Instruct \
     --dataset_name ../datasets/wikitext-2-raw-v1 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_checkpointing \
+    --block_size 512 \
     --do_train \
     --do_eval \
     --num_train_epochs 3 \
     --save_steps 2000 \
-    --output_dir ./tmp/test-clm-medium-xwwxsxv-100
+    --output_dir ./tmp/llama3_wiki2_RoPE \
+    --bf16
 
 # torchrun --nproc_per_node=4 run_clm.py \
 #     --model_name_or_path ../../gpt2-medium \
--- run_qa.sh
+++ run_qa.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 python run_qa.py \
-  --model_name_or_path ../../gpt2-medium \
+  --model_name_or_path ../../Meta-Llama-3-8B-Instruct \
   --dataset_name ../datasets/squad \
   --do_train \
   --do_eval \
-  --per_device_train_batch_size 12 \
+  --per_device_train_batch_size 1 \
   --learning_rate 3e-5 \
   --num_train_epochs 2 \
-  --max_seq_length 384 \
+  --max_seq_length 256 \
   --doc_stride 128 \
   --save_steps 5000 \
-  --output_dir ./tmp/medium_squad_xwwxsxv_10 \
+  --output_dir ./tmp/llama3_squad_RoPE \
+  --bf16 
 
 # torchrun --nproc_per_node=4 run_qa.py \
 #   --model_name_or_path ../../gpt2-large \
